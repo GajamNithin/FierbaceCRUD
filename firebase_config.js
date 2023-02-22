8fae41a2b3a7ddd9a083e7ebeef683ed95bffb61
Nithin Gajam <nithingajam919@gmail.com>
2023-02-22 12:01:58 +0530

    initial commit

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9HOSwnv6Ao9WqRYcjEPDoyGMaWcXqzDM",
+    authDomain: "studentmanagement-65d5e.firebaseapp.com",
+    projectId: "studentmanagement-65d5e",
+    storageBucket: "studentmanagement-65d5e.appspot.com",
+    messagingSenderId: "650387805910",
+    appId: "1:650387805910:web:1aeb323977ef872ad01b77"
+  };
+
+  // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
